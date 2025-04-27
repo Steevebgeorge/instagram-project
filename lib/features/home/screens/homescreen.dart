9cef48b6c2188776f3ff8b9356f7a5ebe57aeaf3
Steevebgeorge <steevebgeorge2292287@gmail.com>
2025-04-27 20:45:39 +0530

    pages added

--- lib/features/home/screens/homescreen.dart
+++ lib/features/home/screens/homescreen.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/features/feed/screens/feedscreen.dart';
+import 'package:instagram/features/notification/screens/notification.dart';
+import 'package:instagram/features/profile/screens/profile.dart';
+import 'package:instagram/features/search/screens/searchscreen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  final PageController _pageController = PageController(initialPage: 0);
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void navigationTap(int page) {
+    _pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _selectedIndex = page;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: onPageChanged,
+        physics: BouncingScrollPhysics(),
+        children: [
+          FeedScreen(),
+          SearchScreen(),
+          NotificationScreen(),
+          ProfileScreen(),
+        ],
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 80,
+        child: BottomNavigationBar(
+          fixedColor: Colors.red,
+          backgroundColor: Colors.grey[200],
+          currentIndex: _selectedIndex,
+          onTap: navigationTap,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), label: "Notification"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          ],
+        ),
+      ),
+    );
   }
 }
